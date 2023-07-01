527771a6e0a96ce454554d89a0ad46651c5eb02c
Anastasia Tseveleva <anastacia.tseveleva@yandex.ru>
2023-07-01 14:52:40 +0300

    mesto_new7

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,15 @@ let openPopUp = document.querySelector('.profile__edit-button');
 let closePopUp = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup');
 let submitBtn = document.querySelector('.popup__save-button');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_status');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_status');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status'); 
 
 function openPopup(){
     formElement.classList.add('popup_opened');
-    nameInput.value = profileName.innerHTML;
-    jobInput.value = profileStatus.innerHTML;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileStatus.textContent;
 }
 
 function handleFormSubmit (evt) {
